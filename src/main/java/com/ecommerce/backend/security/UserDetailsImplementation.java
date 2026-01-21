f0ccf22924e833cfb1c85e925997589f00a6dc4a
barash1311 <138241217+barash1311@users.noreply.github.com>
2026-01-21 22:53:43 +0530

    Authentication initialized

---
+++ src/main/java/com/ecommerce/backend/security/UserDetailsImplementation.java
@@ -0,0 +1,77 @@
+package com.ecommerce.backend.security;
+
+import com.ecommerce.backend.entity.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class UserDetailsImplementation implements UserDetails {
+    private static final Long serialVersionUID = 1L;
+    private Long id;
+    private String username;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImplementation build(User user) {
+        List<GrantedAuthority> authorities = user.getUserRoles()
+                .stream()
+                .map(userRole -> new SimpleGrantedAuthority(userRole.getRole().getRoleName().name()))
+                .collect(Collectors.toList());
+
+        return UserDetailsImplementation.builder()
+                .id(user.getUserId())
+                .username(user.getUserName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+    @Override
+    @NonNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDetailsImplementation that)) return false;
+        return Objects.equals(id, that.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
---
+++ src/main/java/com/ecommerce/backend/security/UserDetailsServiceImplementation.java
@@ -0,0 +1,32 @@
+package com.ecommerce.backend.security;
+
+import com.ecommerce.backend.entity.user.User;
+import com.ecommerce.backend.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User Not Found with username: " + username));
+
+        return UserDetailsImplementation.build(user);
+    }
+
+
+
+}
